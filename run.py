90d4d7caabc4cea24e6e3993754db8323086e968
Nikola <nikola@Calcifer>
2016-10-27 14:01:57 +0100

    updating the pul request options

--- run.py
+++ run.py
@@ -2,9 +2,7 @@ from twython import Twython
 import datetime
 import time
 import sys
-import csv
 import simplekml
-import subprocess
 
 import version
 from config import TGLP_Config
@@ -12,21 +10,27 @@ from config import TGLP_Config
 print version.__banner__
 print "-"*40
 
-twitter_username = TGLP_Config.target_twitter_username
+# twitter_username = TGLP_Config.target_twitter_username
+twitter_username = raw_input("Enter the twitter username to investigate:\n")
 
-print(">>> Requesting {} tweets").format(twitter_username)
+while True:
+    try:
+        number_of_pull_cycles = int(float(raw_input("Enter the number of tweets to pull:\n"))) / 200
+    except ValueError:
+        print("Invalid input, please a valid number.")
+        continue
+    else:
+        break
 
-def output_path_gen():
-    # print ">>> Generating CSV file"
-    ts = time.time()
-    myTimestamp = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y_%H-%M-%S')
+#increments in multiples of 16 (api request limit per 5 mins)
+#and calculates the number of pulls required to the closest 16 multiple
+for i in range(15,1000,15):
+    if number_of_pull_cycles <= i:
+        number_of_pull_cycles = i
+        break
 
-    #save in a folder called kml_files
-    newFileName = "kml_files/"+twitter_username+'-'+myTimestamp+'.kml'
-
-    # bashCmd = 'touch kml_files/{}'.format(newFileName)
-    # subprocess.call(bashCmd, shell=True)
-    return newFileName
+print("-"*40)
+print(">>> Getting {}'s tweets over {} requests").format(twitter_username, number_of_pull_cycles)
 
 CONSUMER_KEY = TGLP_Config.CONSUMER_KEY
 CONSUMER_SECRET = TGLP_Config.CONSUMER_SECRET
@@ -51,7 +55,7 @@ geo_location_tweets = []
 #create kml object to store results
 kml=simplekml.Kml()
 
-for i in range(0, TGLP_Config.number_of_pull_cycles): ## iterate through all tweets
+for i in range(0, number_of_pull_cycles): ## iterate through all tweets
 # tweet extract method with the last list item as the max_id
     user_timeline = twitter.get_user_timeline(screen_name= twitter_username,
         count=200,
@@ -61,19 +65,21 @@ for i in range(0, TGLP_Config.number_of_pull_cycles): ## iterate through all twe
     # print(">>> Searching for a max id of {}").format(LT_ID[-1])
     # print(">>> Successfully gathered {} tweets").format(len(user_timeline))
 
-    #time.sleep(300) #sleep inbetween requests for larger data sets
+    # if i == 0:
+    #     continue
+    # if i % 16 == 0:
+    #     print("+++ 16 request API limit hit on cycle num {}, 5 minrest").format(i)
+    #     time.sleep(300)
 
     for tweet in user_timeline:
         #add the tweets checked to the array
         LT_ID.append(tweet["id"])
 
         #checks if there is a geo location returned
-        if tweet['coordinates'] is None:
-            continue
-
-        print(">>> Tweet id {} has geo data!").format(tweet["id"])
-        # print(tweet)
-        geo_location_tweets.append(tweet) # append tweet id's
+        if tweet['coordinates'] is not None:
+            print(">>> Tweet id {} has geo data!").format(tweet["id"])
+            # print(tweet)
+            geo_location_tweets.append(tweet) # append tweet id's
 
 print(">>> Parsed a total of {} tweets").format(len(LT_ID))
 print(">>> Found {} geo location tweets").format(len(geo_location_tweets))
@@ -84,6 +90,14 @@ for geo_tweet in geo_location_tweets:
         description=geo_tweet["text"],
         coords=[(geo_tweet["coordinates"]["coordinates"][0],
             geo_tweet["coordinates"]["coordinates"][1])])
+
+def output_path_gen():
+    ts = time.time()
+    myTimestamp = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y_%H-%M-%S')
+
+    #save in a folder called kml_files
+    newFileName = "kml_files/"+twitter_username+'-'+myTimestamp+'.kml'
+    return newFileName
 
 kml.save(output_path_gen())
 print(">>> Successfully created kml log file!")
